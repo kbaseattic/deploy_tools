283908f4c5250e4909c4795c80807c2322db0d38
Shane Canon <scanon@lbl.gov>
2013-05-29 17:07:12 +0000

    Added prod tag option and other cleanup to tag.sh

--- tag.sh
+++ tag.sh
@@ -1,24 +1,60 @@
 #!/bin/sh
 
+prod_tag () {
+  SERV=$CW/services
+  cd $TDIR
+  tag=${1}
+  echo "Production Tag: $tag"
+  for module in $(cat $SERV|awk -F: '{print $3}') ; do
+    echo $module
+    if [ ! -e "$module" ] ; then
+      git clone ssh://kbase@git.kbase.us/$module
+    fi
+    cd $module
+    git checkout -q master
+    git pull -q
+    VERS=$(cat $SERV|grep ":${module}:"|awk -F: '{print $9}')
+    git checkout -q $VERS || exit
+    git tag $tag
+    git push --tags
+    cd ..
+  done
+  return 0
+}
 
-if [ $(echo ${1}|grep -c '^20') -gt 0 ] ; then
+# Create workspace
+CW=$(pwd)
+TDIR=/tmp/tagdir
+[ -d $TDIR ] || mkdir $TDIR
+
+if [ "${1}" = "-p" ] ; then
+  echo "Tagging for production"
+  shift
+  if [ $# -gt 0 ] ; then
+    tag=${1}
+  else
+    tag="$(date +%Y%m%d)-prod"
+  fi
+  prod_tag $tag
+  exit
+elif [ $(echo ${1}|grep -c '^20') -gt 0 ] ; then
   tag=${1}
   shift
 else
   tag="$(date +%Y%m%d)-test"
 fi
+
 echo "Tag: $tag"
-
-TDIR=/tmp/tagdir
-
-[ -d $TDIR ] || mkdir $TDIR
 cd $TDIR
 
 for p in $@ ; do
- git clone ssh://kbase@git.kbase.us/$p
+ if [ ! -e "${p}" ] ; then
+   git clone ssh://kbase@git.kbase.us/$p
+ fi
  cd $p
+ git checkout master
+ git pull
  git tag $tag
  git push --tags
  cd ..
-# rm -rf $p
 done
