6f730c4c1c718ec8dd38104394589ec534a85355
root <root@install.(none)>
2014-06-13 17:29:04 -0700

    - Created KBDeploy.pm and moved some common functions to it - Created setup_service.  Basic functionality working. - Added various helper scripts to make setup_service work

---
+++ perl/KBDeploy.pm
@@ -0,0 +1,56 @@
+package KBDeploy;
+
+use strict;
+use warnings;
+use Config::IniFiles;
+use Switch;
+use Data::Dumper;
+
+use Carp;
+use Exporter;
+
+$KBDeploy::VERSION = "1.0";
+
+use vars  qw(@ISA @EXPORT_OK);
+use base qw(Exporter);
+
+our @ISA    = qw(Exporter);
+our @EXPORT = qw(read_config mysystem);
+
+sub read_config {
+   my $file=shift;
+   my $globaltag=shift;
+   my $cfg;
+   my $mcfg=new Config::IniFiles( -file => $file) or die "Unable to open $file".$Config::IniFiles::errors[0];
+
+   # Could use the tie option, but let's build it up ourselves
+
+   for my $section ($mcfg->Sections()){
+     if ($section eq $globaltag){
+       foreach ($mcfg->Parameters($section)){
+         $cfg->{$globaltag}->{$_}=$mcfg->val($section,$_);
+       }
+     }
+     else {
+       $cfg->{services}->{$section}->{mem}=$cfg->{$globaltag}->{mem};
+       $cfg->{services}->{$section}->{cores}=$cfg->{$globaltag}->{cores};
+       $cfg->{services}->{$section}->{host}=$cfg->{$globaltag}->{basename}."-".$section;
+       foreach ($mcfg->Parameters($section)){
+         $cfg->{services}->{$section}->{$_}=$mcfg->val($section,$_);
+       }
+       if (! defined $cfg->{services}->{$section}->{type} || $cfg->{services}->{$section}->{type} ne 'lib'){
+         push @{$cfg->{servicelist}},$section;
+       }
+     }
+   }
+  return $cfg;
+}
+
+
+sub mysystem {
+  foreach (@_){
+    system($_) eq 0 or die "Failed on $_ in $0\n";
+  }
+}
+
+1;
