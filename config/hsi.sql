7fce4c2075b92ee2821019fb7550c8af2a7dfc81
Shane Canon <scanon@lbl.gov>
2014-07-07 17:00:10 -0700

    Converted mysql setup to a perl script.  Added an option for special service sql scripts.

---
+++ config/hsi.sql
@@ -0,0 +1,12 @@
+USE hsi;
+CREATE TABLE IF NOT EXISTS Handle (
+        id              varchar(256) NOT NULL DEFAULT '',
+        file_name     varchar(256),
+        type            varchar(256),
+        url             varchar(256),
+        remote_md5      varchar(256),
+        remote_sha1     varchar(256),
+        created_by      varchar(256),
+        creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (id)
+); 
