5611b6221b65b0407a57a6e1fa41d95b63e722af
Shane Canon <scanon@lbl.gov>
2014-12-17 13:22:52 -0800

    - Generalized DeployAll.sh - General cleanup - Added postprocess scripts to repo - Added geerate_host helper script - Added narrative.nginx example

--- DeployAll.sh
+++ DeployAll.sh
@@ -2,11 +2,11 @@
 
 PATH=$PATH:/sbin
 
-label='next'
+label=$(grep basename cluster.ini|sed 's/.*=//')
 
 . /etc/profile.d/xcat.sh
 
-cd /root/deploy_tools
+cd $(readlink -f $(dirname $0))
 ./deploy_cluster resetdeploy
 # there are race conditions all over the place here, just ignoring for now
 ./deploy_cluster deploy all |grep -v 'Sending build context to Docker daemon'| tee /tmp/deploy.report.$label
@@ -14,15 +14,7 @@ echo "Check status of services: https://monitor.kbase.us/check_mk/view.py?view_n
 echo >> /tmp/deploy.report.$label.mail
 grep 'Services deployed successfully' /tmp/deploy.report.$label >> /tmp/deploy.report.$label.mail
 # UUOC
-cat /tmp/deploy.report.$label.mail | mail -r 'nightly-build@kbase.us' -s "$label.kbase.us Nightly Build Report" kkeller@lbl.gov scanon@lbl.gov dolson@mcs.anl.gov nightly-build@lists.kbase.us
+#cat /tmp/deploy.report.$label.mail | mail -r 'nightly-build@kbase.us' -s "$label.kbase.us Nightly Build Report" kkeller@lbl.gov scanon@lbl.gov dolson@mcs.anl.gov nightly-build@lists.kbase.us
+cat /tmp/deploy.report.$label.mail | mail -r 'nightly-build@kbase.us' -s "$label.kbase.us Nightly Build Report" kkeller@lbl.gov
 # just in case -r doesn't work in cron job
 #cat /tmp/deploy.report.mail | mail -s 'next.kbase.us Nightly Build Report' kkeller@lbl.gov 
-
-logdir=/tmp/deploy_logs/$label/$(date +%F)
-mkdir -p $logdir
-# might be an xcatty-way to do this
-for node in $(nodels $label)
-do
-  scp $node:/tmp/deploy.log $logdir/$node.deploy.log
-  scp $node:/tmp/test.log $logdir/$node.test.log
-done
